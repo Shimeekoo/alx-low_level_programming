49a09a4c31298ab628443b34747fccac457436c9
Shimeekoo <shimestesfa82@gmail.com>
2022-06-08 02:46:57 -0700

    task 7 updated

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,22 +11,13 @@ int print_last_digit(int n)
 
 		int last_digit;
 
-
-
 		if (n < 0)
-
 			last_digit = (n % 10) * -1;
-
 		else
-
 			last_digit = n % 10;
 
+		_putchar(last_digit + '0');
 
-
-			_putchar(last_digit + '0');
-
-
-
-			return (last_digit);
+		return (last_digit);
 
 }
