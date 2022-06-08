92c194b2705c18d95b62d08521d2cd44251a66f8
Shimeekoo <shimestesfa82@gmail.com>
2022-06-08 07:58:42 -0700

    task 0 updated

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,6 @@
-#ifndef main_H
+#ifndef MAIN_H
 
-#define main_H
+#define MAIN_H
 
 
 
@@ -15,6 +15,8 @@ int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 
 void print_remaining_days(int month int day, int year);
+
+void print_times_table(int n);
 
 
 
