5af32f75a74a8f1738359e4805f48561aafe72e0
Shimeekoo <shimestesfa82@gmail.com>
2022-06-09 08:38:33 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -8,14 +8,10 @@
 int main(void)
 
 {
-	int n;
-
-	if (n < 0)
-		printf("%d is negative\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
-		printf("%d is positive\n", n);
+	int i;
+	
+	i = 0;
+	positive_or_negative(i);
 
 	return (0);
 }
